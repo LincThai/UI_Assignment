b447d69d638d28a7bab2e0b139791f9c00ce05f7
LincThai <78945845+LincThai@users.noreply.github.com>
2025-06-02 17:40:35 +1000

    Add Scriptable objects

---
+++ Assets/Scripts/SkillScroll.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+
+public class SkillScroll : MonoBehaviour
+{
+    // set variables
+    public Skill skill;
+    public TMP_Text skillNameText;
+    public TMP_Text skillDescText;
+
+    bool learned;
+    bool unlocked;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
